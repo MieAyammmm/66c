06685b8fd07e308c3b0f0167556461a491f51a3b
dani <dhannizd2@gmail.com>
2025-06-23 13:46:07 +0700

    setting db

--- api/index.js
+++ api/index.js
@@ -6,6 +6,11 @@ app.get("/api", (c) => {
   return c.text("hi");
 });
 
+app.get("/api/transactions", async (c) => {
+  let { results } = await c.env.DB.prepare("SELECT * FROM transactions").all();
+  return c.json(results);
+});
+
 app.get("*", (c) => c.env.ASSETS.fetch(c.req.raw));
 
 export default app;
---
+++ migrations/0001_create_transactions.sql
@@ -0,0 +1,9 @@
+-- Migration number: 0001 	 2025-06-23T06:40:06.536Z
+CREATE TABLE IF NOT EXISTS transactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    price INTEGER
+    category TEXT
+);
+
+INSERT INTO transactions,(name, price, category) VALUES ('Cilok', 5000, 'Maem'), ('Batagor', 6000, 'Maem');
