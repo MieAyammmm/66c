e23dc4bddb158e0097ed07a8b973c3228f21714d
dani <dhannizd2@gmail.com>
2025-06-23 13:55:25 +0700

    fix error db

--- migrations/0001_create_transactions.sql
+++ migrations/0001_create_transactions.sql
@@ -1,9 +1,10 @@
--- Migration number: 0001 	 2025-06-23T06:40:06.536Z
+-- Migration number: 0001 	 2025-06-23T06:54:25.271Z
 CREATE TABLE IF NOT EXISTS transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    price INTEGER
+    price INTEGER,
     category TEXT
 );
 
-INSERT INTO transactions,(name, price, category) VALUES ('Cilok', 5000, 'Maem'), ('Batagor', 6000, 'Maem');
+INSERT INTO transactions(name, price, category) 
+VALUES ('Cilok', 5000, 'Maem'), ('Batagor', 6000, 'Maem');
